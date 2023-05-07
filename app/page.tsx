2d9404d76d5c776d5e1369eff3e131f0146472b9
chadittya <chadittya@gmail.com>
2023-05-08 00:20:52 +0700

    finishing mobile layout and projects

--- app/page.tsx
+++ app/page.tsx
@@ -105,18 +105,18 @@ export default function Home() {
   };
   return (
     <main className="flex justify-center items-center min-h-screen">
-      <div className="bg-white rounded-t-xl rounded-bl-xl rounded-br-[5rem] max-w-md relative">
-        <div className="mx-10 my-10">
+      <div className="bg-white rounded-t-xl rounded-bl-xl rounded-br-[5rem] md:max-w-md min-w-min relative md:mx-0 mx-2">
+        <div className="md:mx-10 mx-3 md:my-10 my-3">
           <form
             onSubmit={handleSubmit}
-            className="flex flex-row text-slate-500 gap-3 mb-7"
+            className="flex flex-row text-slate-500 gap-3 md:mb-7 mb-9"
           >
             <div className="flex flex-col gap-1 relative">
-              <label className="text-[0.4rem] tracking-[0.5em] uppercase">
+              <label className="md:text-[0.4rem] text-[0.3rem] tracking-[0.5em] uppercase">
                 Day
               </label>
               <input
-                className="border border-slate-400 rounded-[8px] max-w-[150px] py-1 px-2 outline-none focus:border-chPurple text-black"
+                className="border border-slate-400 rounded-[8px] md:max-w-[150px] max-w-[75px] md:py-1 py-2 px-2 outline-none focus:border-chPurple text-black md:text-base text-[0.4rem]"
                 type="number"
                 min={0}
                 max={31}
@@ -125,22 +125,22 @@ export default function Home() {
                 onChange={(e) => setDay(parseInt(e.target.value))}
               />
               {!isValid && (
-                <span className="text-chLightRed italic text-[0.42rem] absolute -bottom-3">
+                <span className="text-chLightRed italic md:text-[0.42rem] text-[0.35rem] absolute md:-bottom-3 -bottom-5">
                   This field is required
                 </span>
               )}
               {day && (day < 1 || day > 31) && (
-                <span className="text-chLightRed italic text-[0.42rem] absolute -bottom-3">
+                <span className="text-chLightRed italic md:text-[0.42rem] text-[0.35rem] absolute md:-bottom-3 -bottom-5">
                   Must be valid day
                 </span>
               )}
             </div>
             <div className="flex flex-col gap-1 relative">
-              <label className="text-[0.4rem] tracking-[0.5em] uppercase">
+              <label className="md:text-[0.4rem] text-[0.3rem] tracking-[0.5em] uppercase">
                 Month
               </label>
               <input
-                className="border border-slate-400 rounded-[8px] max-w-[150px] py-1 px-2 outline-none focus:border-chPurple text-black"
+                className="border border-slate-400 rounded-[8px] md:max-w-[150px] max-w-[75px] md:py-1 py-2 px-2 outline-none focus:border-chPurple text-black md:text-base text-[0.4rem]"
                 type="number"
                 min={0}
                 max={12}
@@ -149,29 +149,29 @@ export default function Home() {
                 onChange={(e) => setMonth(parseInt(e.target.value))}
               />
               {!isValid && (
-                <span className="text-chLightRed italic text-[0.42rem] absolute -bottom-3">
+                <span className="text-chLightRed italic md:text-[0.42rem] text-[0.35rem] absolute md:-bottom-3 -bottom-5">
                   This field is required
                 </span>
               )}
               {month && (month < 1 || month > 12) && (
-                <span className="text-chLightRed italic text-[0.42rem] absolute -bottom-3">
+                <span className="text-chLightRed italic md:text-[0.42rem] text-[0.35rem] absolute md:-bottom-3 -bottom-5">
                   Must be valid month
                 </span>
               )}
               {year === new Date().getFullYear() &&
                 month &&
                 month > new Date().getMonth() + 1 && (
-                  <span className="text-chLightRed italic text-[0.42rem] absolute -bottom-6">
+                  <span className="text-chLightRed italic md:text-[0.42rem] text-[0.35rem] absolute -bottom-6">
                     Must be a valid Month in the past
                   </span>
                 )}
             </div>
-            <div className="flex flex-col gap-1 mr-12 relative">
-              <label className="text-[0.4rem] tracking-[0.5em] uppercase">
+            <div className="flex flex-col gap-1 md:mr-12 relative">
+              <label className="md:text-[0.4rem] text-[0.3rem] tracking-[0.5em] uppercase">
                 Year
               </label>
               <input
-                className="border border-slate-400 rounded-[8px] max-w-[150px] py-1 px-2 outline-none focus:border-chPurple text-black"
+                className="border border-slate-400 rounded-[8px] md:max-w-[150px] max-w-[75px] md:py-1 py-2 px-2 outline-none focus:border-chPurple text-black md:text-base text-[0.4rem]"
                 type="number"
                 min={1900}
                 max={new Date().getFullYear()}
@@ -180,28 +180,28 @@ export default function Home() {
                 onChange={(e) => setYear(parseInt(e.target.value))}
               />
               {!isValid && (
-                <span className="text-chLightRed italic text-[0.42rem] absolute -bottom-3">
+                <span className="text-chLightRed italic md:text-[0.42rem] text-[0.35rem] absolute md:-bottom-3 -bottom-5">
                   This field is required
                 </span>
               )}
               {year && year > new Date().getFullYear() && (
-                <span className="text-chLightRed italic text-[0.42rem] absolute -bottom-6">
+                <span className="text-chLightRed italic md:text-[0.42rem] text-[0.35rem] absolute -bottom-6">
                   Must be a valid year in the past
                 </span>
               )}
             </div>
-            <div className="absolute right-6 top-[5.9rem]">
+            <div className="absolute md:right-6 md:top-[5.9rem] top-[4.3rem] right-[4.4rem]">
               <button type="submit">
                 <Image
                   src={iconArrow}
                   alt=""
-                  className="bg-chPurple rounded-full w-11 px-2 py-2 hover:bg-black active:bg-chLightGrey active:text-black transition-all"
+                  className="bg-chPurple rounded-full md:w-11 w-9 px-2 py-2 hover:bg-black active:bg-chLightGrey active:text-black transition-all"
                 />
               </button>
             </div>
           </form>
           <hr className="mb-6" />
-          <div className="flex flex-col font-extrabold italic text-5xl">
+          <div className="flex flex-col font-extrabold italic md:text-5xl text-2xl">
             <label>
               <span
                 className={`text-chPurple ${showAge ? "animate-bounce" : ""}`}
